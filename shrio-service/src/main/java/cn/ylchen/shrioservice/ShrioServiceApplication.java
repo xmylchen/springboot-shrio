962727c95d64517d0b0b34e4424f39d54b8e8b22
xmylchen <cylin600@163.com>
2018-12-16 14:26:35 +0800

    first commit test

---
+++ shrio-service/src/main/java/cn/ylchen/shrioservice/ShrioServiceApplication.java
@@ -0,0 +1,14 @@
+package cn.ylchen.shrioservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrioServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShrioServiceApplication.class, args);
+    }
+
+}
+
