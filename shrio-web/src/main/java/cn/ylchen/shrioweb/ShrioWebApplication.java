6f281a9e5f15620ca8da88ffb651910625a0c541
xmylchen <cylin600@163.com>
2018-12-18 21:07:50 +0800

    add web

---
+++ shrio-web/src/main/java/cn/ylchen/shrioweb/ShrioWebApplication.java
@@ -0,0 +1,14 @@
+package cn.ylchen.shrioweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShrioWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShrioWebApplication.class, args);
+    }
+
+}
+
